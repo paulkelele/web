494665c6471c536f712602b39fb21f162d8d1f0c
stef boy <stef@exemple.com>
2022-08-29 14:36:18 +0200

    change

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	_ "github.com/go-sql-driver/mysql"
-
+	"log"
 	// "WebApp/toto"
 )
+
 // pour importer "APP/NOMPACKAGE"
 // puis NOMPACKAGE.nomMethode  qui est dans un fichier...Le nom du fichier dans
 // le package on s'en fou
@@ -18,34 +18,30 @@ import (
 
 //  site pour mysql documentation => https://zetcode.com/golang/mysql/
 
-// les commandes: 
+// les commandes:
 //				db.QueryRow().Scan() => query une seule ligne => renvoit *Row
 //				db.Query => query sur toutes les lignes => renvoit *Row, err
 // 				db.Exec(sql) => INSERT || DELETE => renvoit *Row, err
 // prepared statement : db.Exec("...?", varOf?)
 
+func main() {
 
-
-func main(){
- 
-	db, err := sql.Open("mysql","root:cerise@tcp(localhost:3306)/toto")
+	db, err := sql.Open("mysql", "root:cerise@tcp(localhost:3306)/toto")
 	defer db.Close()
-	if err != nil{
+	if err != nil {
 		log.Fatal((err))
 	}
 
-	sql :="insert into user(nom,prenom,age) values(?,?,?)"
-	 
-res, err2 :=db.Exec(sql,"MARTIN","PAUL",44)
-if err2 != nil{
-	log.Fatal(err2)
-}
- LastId, err3 := res.LastInsertId()
+	sql := "insert into user(nom,prenom,age) values(?,?,?)"
+	res, err2 := db.Exec(sql, "MARTIN", "PAUL", 44)
 
-if err3 != nil{
-	log.Fatal((err3.Error()))
-}
- fmt.Println("Les tid inseert: ",LastId)
-}
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	LastId, err3 := res.LastInsertId()
 
- 
+	if err3 != nil {
+		log.Fatal((err3.Error()))
+	}
+	fmt.Println("Les tid inseert: ", LastId)
+}
