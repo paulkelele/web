edf655302703c6d870da43ad433c0e176d7ee256
stef boy <stef@exemple.com>
2022-08-29 12:16:58 +0200

    connection mysql

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+
 	// "WebApp/toto"
 )
 // pour importer "APP/NOMPACKAGE"
-// puis NOMPACKAGE.nomMethode ...Le nom du fichier dans
+// puis NOMPACKAGE.nomMethode  qui est dans un fichier...Le nom du fichier dans
 // le package on s'en fou
 
 // liste des build possibles => go tool dist list
@@ -13,9 +17,23 @@ import (
 // pour le build => GOOS=windows GOARCH=386 go build .  => pour Windows
 
 func main(){
-	// toto.Imprime()
-	// toto.Reimprime()
-	fmt.Println("eeeee")
+ 
+	db, err := sql.Open("mysql","root:cerise@tcp(localhost:3306)/toto")
+	defer db.Close()
+	if err != nil{
+		log.Fatal((err))
+	}
+	var version string
+
+	err2 := db.QueryRow("SELECT VERSION()").Scan((&version))
+
+	if err2 != nil{
+		log.Fatal(err2)
+	}
+
+	fmt.Println(version)
+
+	 
 }
 
  
